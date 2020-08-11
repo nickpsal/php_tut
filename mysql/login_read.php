ee50c7d1059626ea83d12d4865f83b035392a2dc
NIKOLAOS PSALTAKIS <nickpsal@gmail.com>
2020-08-11 11:36:25 +0300

    11/08/2020

--- mysql/login_read.php
+++ mysql/login_read.php
@@ -5,10 +5,6 @@
 ?>
     <?php include "includes/header.php"?>;
     <div class="container">
-<<<<<<< HEAD
-=======
-    <H1 class="text-center">ALL USERS</H1>
->>>>>>> 89501b9b875ab763ac672712a03cce2cd0b45cd6
         <div class="col-sm-6">
         </div>
     </div>
